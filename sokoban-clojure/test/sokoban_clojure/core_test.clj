dcdde3232b3056a49e63b34a46ae86d067425290
bhayward93 <B34nsprout>
2016-11-29 21:39:08 +0000

    RED NAMES GO AWAY

---
+++ sokoban-clojure/test/sokoban_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns sokoban-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [sokoban-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
