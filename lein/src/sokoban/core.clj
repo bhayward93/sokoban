a195167e2d768e62990ad21b2894ffe92ab9a702
bhayward93 <B34nsprout>
2016-12-13 17:23:49 +0000

    Files can now see eachother. Yey!

--- lein/src/sokoban/core.clj
+++ lein/src/sokoban/core.clj
@@ -1,7 +1,8 @@
 (ns sokoban.core
-  (:gen-class))
+  (:gen-class)
+  (require '[cgsx.tools.matcher :refer :all]))
 
-(load-file "../sock2/socket.clj")
+(load-file "./src/sock2/socket.clj")
 (def s25 (startup-server 2222))
 
 (defn -main
