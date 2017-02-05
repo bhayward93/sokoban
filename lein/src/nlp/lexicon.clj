b00d5fa74f169e7f0c4d53866db9635564774edf
bhayward93 <B34nsprout>
2017-02-05 20:59:06 +0000

    Not much here, uploading something to discuss in the lexicon

--- lein/src/nlp/lexicon.clj
+++ lein/src/nlp/lexicon.clj
@@ -4,4 +4,10 @@
               :block noun, :switch noun, :agent noun,
               :worker noun, })
 
+;the colors used in the nlp. these can change or be added to but
+;need to be fed in with the lexicon. Note that colours can be nouns or adjectives.
+(def colors (:red :orange :green))
+
+(def sufixes (:ly :dy)) ;ect
+
 ;note, switch could be taken multiple ways
--- lein/src/nlp/morphologicalprocessor.clj
+++ lein/src/nlp/morphologicalprocessor.clj
@@ -1,5 +1,11 @@
 (ns nlp.morphologicalprocessor)
 
+;Gets the users input
+(defn get-input []
+
+  )
+
+;Example input: '(Push the blue block forward)
 ;pick apart words by looking for common suffixes and
 ;prefixes, and then transform the words accordingly
 
