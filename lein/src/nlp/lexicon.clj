dd14cbc52cbf569778a8f4a308ebe4e08ad64d05
bhayward93 <B34nsprout>
2017-02-07 19:58:28 +0000

    Added prefixes and suffixes to the lexicon; data may be of the wrong shape atm but we can use the REPL to convert the structure anyway. Also lots of notes and considerations to look at in the code.

--- lein/src/nlp/lexicon.clj
+++ lein/src/nlp/lexicon.clj
@@ -1,13 +1,33 @@
 (ns nlp.lexicon)
 ;stores the words known
-(def lexicon {:box noun , :move verb , :push verb , :gently adjective ,
+(def lexicon {:box noun ,   :block noun,  :square noun,:object noun, ;for box
+              :move verb,   :push verb,   :walk verb,  :run verb         ;for moving
+              :worker noun, :player noun, :agent noun, :man noun, :woman, :person noun, :guy noun, :character noun ;the player
+              :gentle adjective , :force verb
               :block noun, :switch noun, :agent noun,
               :worker noun, })
 
-;the colors used in the nlp. these can change or be added to but
-;need to be fed in with the lexicon. Note that colours can be nouns or adjectives.
-(def colors (:red :orange :green))
 
-(def sufixes (:ly :dy)) ;ect
+(def colors (:red :orange :green));
 
-;note, switch could be taken multiple ways
+;suffixes - note: if someone uses the word size, will it find ize in the suffixes list,
+;           and wrongly deduce its meaning.
+(def sufixes (:acy :al :ance :dom :er :or :ism :ist :ty
+               :ment :ness :ship :sion :tion :ate :en
+               :ify :fy :ize :ise :able :ible :al :esque
+               :ful :ic :ical :ious :ous :ish :ive :less :y))
+;taken from http://grammar.about.com/od/words/a/comsuffixes.htm
+
+;prefixes - note, can convert this and suffixes into a different datatype using REPL.
+;           should we be storing the values of these symbols in a map, with the value being the meaning
+(defn prefixes (:anti :de :dis :en :em :fore :in :im :in
+                 :im :il :ir :inter :mid :mis :non :over
+                 :pre :re :semi :sub :super :trans :un :under))
+
+;taken from http://teacher.scholastic.com/reading/bestpractices/vocabulary/pdf/prefixes_suffixes.pdf
+;Considerations:
+;-Group words by type; efficiency?
+;-Need to scan lexicon fully every time, to pick up ambiguty.
+;-the colors used in the nlp. these can change or be added to but
+;-need to be fed in with the lexicon. Note that colours can be nouns or adjectives.
+;-switch could be taken multiple ways
